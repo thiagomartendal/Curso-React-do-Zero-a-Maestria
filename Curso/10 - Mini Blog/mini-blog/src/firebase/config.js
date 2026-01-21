fd374b6b3fe6764e738e5b15a56f3c13f7f48983
thiagomartendal <thiagomartendal@gmail.com>
2026-01-21 16:28:50 -0300

    Add files via upload

---
+++ Curso/10 - Mini Blog/mini-blog/src/firebase/config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+}; // As credenciais originais foram removidas por segurança
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+const auth = getAuth(app)
+
+export { db, auth }
